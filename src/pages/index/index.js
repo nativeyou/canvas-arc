ec9125d2479477d121ea882711227fde1777e162
gaoxuemeng <gaoxuemeng001@huazhu001.com>
2018-12-29 18:10:51 +0800

    update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,22 +4,27 @@ import * as utils from '../../common/js/utils';
 
 const canvas = $('#circle');
 const ctx = canvas.get(0).getContext('2d');
-ctx.globalCompositeOperation="source-atop";
 
-let x = 120,
-    y = 120,
+const ratio = 240 / 375;
+const scale = document.defaultView.devicePixelRatio || 1;
+let x = document.defaultView.innerWidth * ratio * scale / scale / 2,
+    y = document.defaultView.innerWidth * ratio * scale / scale / 2,
     timer = null,
-    radius = 110,
+    radius = (document.defaultView.innerWidth * ratio * scale / scale / 2) * (110/120),
     angleStart = -90,
     angleEnd = 270;
+document.write(x + ', ' + y + ', ' + radius + ', ' + scale + ', ' + radius);
+canvas.get(0).width = Math.floor(document.defaultView.innerWidth * ratio * scale);
+canvas.get(0).height = Math.floor(document.defaultView.innerWidth * ratio * scale);
+canvas.get(0).style.width = canvas.get(0).width / scale + 'px';
+canvas.get(0).style.height = canvas.get(0).width / scale + 'px';
+ctx.scale(scale, scale);
+ctx.translate(-0, -0);
+// ctx.textBaseline = 'bottom';
 
 const dToR = function(degrees){
     return degrees * (Math.PI / 180);
 };
-
-canvas.get(0).width = 2 * canvas.get(0).width;
-canvas.get(0).height = 2 * canvas.get(0).height;
-
 const clear = () => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
@@ -27,27 +32,24 @@ const clear = () => {
 const renderCircleBg = () => {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2*Math.PI, false);
-    ctx.lineWidth = 14;
+    ctx.lineWidth = 14 * ratio;
     ctx.strokeStyle = '#dfeeff';
     ctx.shadowColor = "rgba(0, 0, 0, 0.06)";
-    ctx.shadowBlur = 10;
+    ctx.shadowBlur = 10 * ratio;
     ctx.stroke();
     ctx.closePath();
 };
 
 const renderCircle = () => {
-    if(angleStart === (-(15000/30000 * 100) * 3.6 + 270)){
-        clearInterval(timer);
-    }
-    const gradient = ctx.createLinearGradient(0, 120, 0, 0);
+    const gradient = ctx.createLinearGradient(0, 120 * ratio, 0, 0);
     gradient.addColorStop(1, '#31CBF5');
     gradient.addColorStop(0, '#169BE8');
 
     ctx.beginPath();
-    angleStart = angleStart + 1;
+    angleStart = angleStart + 5;
     ctx.arc(x, y, radius, dToR(angleStart), dToR(angleEnd));
     ctx.strokeStyle = gradient;
-    ctx.lineWidth = 14;
+    ctx.lineWidth = 14 * ratio;
     ctx.lineCap = 'round';
     ctx.stroke();
     ctx.closePath();
@@ -55,7 +57,7 @@ const renderCircle = () => {
 
 const renderCirWhite = () => {
     ctx.beginPath();
-    ctx.arc(120, 10, 4.5, 0, 2*Math.PI, false);
+    ctx.arc(x, x - radius, 4.5 * ratio, 0, 2*Math.PI, false);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
@@ -66,8 +68,13 @@ const loop = () => {
     renderCircleBg();
     renderCircle();
     renderCirWhite();
+    if(angleStart === (-(15000/30000 * 100) * 3.6 + 270)){
+        return window.cancelAnimationFrame(timer);
+    }
+    timer = window.requestAnimationFrame(loop);
 };
-timer = setInterval(loop, 5);
+
+timer = window.requestAnimationFrame(loop);
 
 if(module.hot){
     require('./index.html');
